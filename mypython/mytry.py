5cdcf656354708d5588dbb6fcbbc6a1117823574
David R <rosales9146@gmail.com>
2018-08-20 19:08:35 +0000

    [REF] Modificado de archivo de Python

--- mypython/mytry.py
+++ mypython/mytry.py
@@ -9,3 +9,8 @@ conv_fac = 0.621371
 # calculate miles
 miles = kilometers * conv_fac
 print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
+
+#esto es un comentario
+"""
+Esto es un comentario
+"""
