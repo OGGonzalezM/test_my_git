4591ae39bebb92949ea2f7d808d82c797771d528
Oscar Gonzalez <ogm@soluciones4g.com>
2018-08-20 19:02:32 +0000

    [REF] mypython: Modified the main python file

--- mypython/mytry.py
+++ mypython/mytry.py
@@ -1,11 +1,24 @@
 kilometers = 5.5
 
+print("Muestra de ejemplo")
+
+
 # To take kilometers from the user, uncomment the code below
-kilometers = input("Enter value in kilometers")
+
+
 
 # conversion factor
 conv_fac = 0.621371
 
+
+# Mi comentario OGGonzalezM
+
 # calculate miles
 miles = kilometers * conv_fac
 print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
+
+
+
+""" README.md
+	Mi comentario externo desde python
+"""
